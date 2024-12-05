cdcc621336c83af4c1e656247def52d471f54337
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2024-12-05 09:27:06 +0530

    removes monolith application and retains microservices

--- FirstJobApplication/src/main/java/com/firstjobapp/FirstJobApplication/Company/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.firstjobapp.FirstJobApplication.Company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company,Long> {
-}
